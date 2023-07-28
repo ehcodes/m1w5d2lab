b297580ee7f2732d91e26b226e78b8492bff3ead
ehcodes <engrid.hernandez@gmail.com>
2023-07-28 13:40:18 -0400

    added darkMode function for li click events

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-const btn = document.querySelector('#commentButton');
-btn.addEventListener('click', function(evt) {
-    const li = document.createElement('li');
-    const inp = document.querySelector('input');
-    li.textContent = inp.value;
-    document.querySelector('ul').appendChild(li);
+const btn = document.querySelector("button");
+const textInput = document.querySelector("input");
+const list = document.querySelector(".list");
+
+list.addEventListener("click", darkMode);
+
+function darkMode(e) {
+    e.target.style.backgroundColor = "black";
+    e.target.style.color = "white";
+}
+
+btn.addEventListener("click", function (evt) {
+  const li = document.createElement("li");
+  const userText = textInput.value;
+  li.textContent = userText;
+  textInput.value = "";
+
+  document.querySelector("ul").appendChild(li);
 });
